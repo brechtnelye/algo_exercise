58eb248ecf5e84014589c82412df8a10e5de8879
Brecht Van Eylen <brecht.vaneylen@student.ucll.be>
2023-04-30 01:55:54 +0200

    used prettier formatting

--- chapters/arithmetic/student.js
+++ chapters/arithmetic/student.js
@@ -1,86 +1,89 @@
 // Schrijf hier je code
 function five() {
-    return 5;
+  return 5;
 }
 
 function average() {
-    return (135 + 471) / 2
+  return (135 + 471) / 2;
 }
 
 function average2(x, y) {
-    return (x+y)/2;
+  return (x + y) / 2;
 }
 
-function average3(x,y,z) {
-    return (x+y+z)/3;
+function average3(x, y, z) {
+  return (x + y + z) / 3;
 }
 
 function distance(x, y) {
-    return (x**2 + y**2)**0.5;
+  return (x ** 2 + y ** 2) ** 0.5;
 }
 
 function busesNeeded(people, busCapacity) {
-    return Math.ceil(people/busCapacity);
+  return Math.ceil(people / busCapacity);
 }
 
 function sugar(pies) {
-    return Math.ceil(pies*0.25);
+  return Math.ceil(pies * 0.25);
 }
 
 function candyPerChild(children, candy) {
-    return Math.floor(candy/children);
+  return Math.floor(candy / children);
 }
 
 function leftoverCandy(children, candy) {
-    return candy % children;
+  return candy % children;
 }
 
 function hours(totalMinutes) {
-    return Math.floor(totalMinutes / 60);
+  return Math.floor(totalMinutes / 60);
 }
 
 function minutes(totalMinutes) {
-    return totalMinutes % 60;
+  return totalMinutes % 60;
 }
 
 function communicationCosts(seconds) {
-    return Math.ceil(seconds/60)*0.2;
+  return Math.ceil(seconds / 60) * 0.2;
 }
 
 function distance2(x1, y1, x2, y2) {
-    let dx = x2 - x1;
-    let dy = y2 - y1;
-    
-    return (dx**2 + dy**2)**0.5;
+  let dx = x2 - x1;
+  let dy = y2 - y1;
+
+  return (dx ** 2 + dy ** 2) ** 0.5;
 }
 
 function cake(eggs) {
-    return Math.floor(eggs / 3);
+  return Math.floor(eggs / 3);
 }
 
 function cake2(eggs, flour) {
-    let minEggs = Math.floor(eggs/4);
-    let minFlour = Math.floor(flour/250);
-    return Math.min(minEggs, minFlour);
+  let minEggs = Math.floor(eggs / 4);
+  let minFlour = Math.floor(flour / 250);
+  return Math.min(minEggs, minFlour);
 }
 
 function cake3(eggs, flour, sugar, butter) {
-    let eggCount = Math.floor(eggs/4);
-    let flourCount = Math.floor(flour/250);
-    let sugarCount = Math.floor(sugar/250);
-    let butterCount = Math.floor(butter/250);
+  let eggCount = Math.floor(eggs / 4);
+  let flourCount = Math.floor(flour / 250);
+  let sugarCount = Math.floor(sugar / 250);
+  let butterCount = Math.floor(butter / 250);
 
-    return Math.min(eggCount, flourCount, sugarCount, butterCount);
-
+  return Math.min(eggCount, flourCount, sugarCount, butterCount);
 }
 
 function roundToMultiple(n, multipleOf) {
-        return Math.round(n / multipleOf) * multipleOf;
+  return Math.round(n / multipleOf) * multipleOf;
 }
 
 function middle(a, b, c) {
-    let min = Math.min(a, b, c);
-    let max = Math.max(a, b, c);
-    
-    return (a+b+c)-min-max;// a, b, c or a, b or a 	// min of a,
+  let min = Math.min(a, b, c);
+  let max = Math.max(a, b, c);
+
+  return a + b + c - min - max; // a, b, c or a, b or a 	// min of a,
+}
+
+function boxArea(width, height, depth) {
+  return (width * height + width * depth + height * depth) * 2;
 }
--- chapters/car/student.js
+++ chapters/car/student.js
@@ -1,267 +1,260 @@
 // Schrijf hier je code
 function myFirstFunction(bike) {
-    forward(bike);
-
+  forward(bike);
 }
 
 function twiceForward(bike) {
-    forward(bike);
-    forward(bike);
+  forward(bike);
+  forward(bike);
 }
 
 function thriceForward(bike) {
-    forward(bike);
-    forward(bike);
-    forward(bike);
+  forward(bike);
+  forward(bike);
+  forward(bike);
 }
 
 function forward4(bike) {
-    forward(bike);
-    forward(bike);
-    forward(bike);
-    forward(bike);
+  forward(bike);
+  forward(bike);
+  forward(bike);
+  forward(bike);
 }
 
 function forward5(bike) {
-    let i = 5;
-    while(i>0) {
-        forward(bike);
-        i--;
-    }
+  let i = 5;
+  while (i > 0) {
+    forward(bike);
+    i--;
+  }
 }
 
 function forward10(bike) {
-    let i = 10;
-    while(i>0) {
-        forward(bike);
-        i--;
-    }
+  let i = 10;
+  while (i > 0) {
+    forward(bike);
+    i--;
+  }
 }
 
 function right(bike) {
-    turnRight(bike);
-    forward(bike);
+  turnRight(bike);
+  forward(bike);
 }
 
 function ellShape(bike) {
-    forward5(bike);
-    turnRight(bike);
-    forward4(bike);
+  forward5(bike);
+  turnRight(bike);
+  forward4(bike);
 }
 
 function uTurn(bike) {
-    thriceForward(bike);
-    turnRight(bike);
-    forward10(bike);
-    turnRight(bike);
-    twiceForward(bike);
+  thriceForward(bike);
+  turnRight(bike);
+  forward10(bike);
+  turnRight(bike);
+  twiceForward(bike);
 }
 
 function forwardN(bike, steps) {
-    let i = steps;
+  let i = steps;
 
-    while(i > 0) {
-        forward(bike);
-        i--;
-    }
+  while (i > 0) {
+    forward(bike);
+    i--;
+  }
 }
 
 function crookedUTurn(bike) {
-    forwardN(bike, 7);
-    turnRight(bike);
-    forwardN(bike, 9);
-    turnRight(bike);
-    forwardN(bike, 3);
+  forwardN(bike, 7);
+  turnRight(bike);
+  forwardN(bike, 9);
+  turnRight(bike);
+  forwardN(bike, 3);
 }
 
 function forwardUntilWall(bike) {
-    while(!sensor(bike)) {
-        forward(bike);
-    }
+  while (!sensor(bike)) {
+    forward(bike);
+  }
 }
 
 function smartEllShape(bike) {
-    forwardUntilWall(bike);
-    turnRight(bike);
-    forwardUntilWall(bike);
+  forwardUntilWall(bike);
+  turnRight(bike);
+  forwardUntilWall(bike);
 }
 
 function spiral(car) {
-    while(!sensor(car)) {
-        forwardUntilWall(car);
-        turnRight(car);
-    }
+  while (!sensor(car)) {
+    forwardUntilWall(car);
+    turnRight(car);
+  }
 }
 
 function turnLeft(car) {
-    turnRight(car);
-    turnRight(car);
-    turnRight(car);
+  turnRight(car);
+  turnRight(car);
+  turnRight(car);
 }
 
 function left(car) {
-    turnLeft(car);
-    forward(car);
+  turnLeft(car);
+  forward(car);
 }
 
 function slalom(car) {
-    forwardUntilWall(car);
-    turnLeft(car);
-    forwardUntilWall(car);
-    turnRight(car);
-    forwardUntilWall(car);
-    turnRight(car);
-    forwardUntilWall(car);
-    turnLeft(car);
-    forwardUntilWall(car);
-    turnLeft(car);
-    forwardUntilWall(car);
-    turnRight(car);
-    forwardUntilWall(car);
-    turnRight(car);
-    forwardUntilWall(car);
+  forwardUntilWall(car);
+  turnLeft(car);
+  forwardUntilWall(car);
+  turnRight(car);
+  forwardUntilWall(car);
+  turnRight(car);
+  forwardUntilWall(car);
+  turnLeft(car);
+  forwardUntilWall(car);
+  turnLeft(car);
+  forwardUntilWall(car);
+  turnRight(car);
+  forwardUntilWall(car);
+  turnRight(car);
+  forwardUntilWall(car);
 }
 
 function leftOrRight(car) {
-    turnLeft(car);
-    forwardUntilWall(car);
-    turnRight(car);
-    forwardUntilWall(car);
-    turnRight(car);
-    forwardUntilWall(car);
-    turnLeft(car);
-    forwardUntilWall(car);
+  turnLeft(car);
+  forwardUntilWall(car);
+  turnRight(car);
+  forwardUntilWall(car);
+  turnRight(car);
+  forwardUntilWall(car);
+  turnLeft(car);
+  forwardUntilWall(car);
 }
 
 function incompleteU(car) {
-    
-    forwardUntilWall(car);
-    turnRight(car);
-    forwardUntilWall(car);
-    turnRight(car);
-    forwardUntilWall(car);
+  forwardUntilWall(car);
+  turnRight(car);
+  forwardUntilWall(car);
+  turnRight(car);
+  forwardUntilWall(car);
 }
 
 function whichDirection(car) {
-    while(sensor(car)) {
-        turnRight(car);
-    }
-    forwardUntilWall(car);
+  while (sensor(car)) {
+    turnRight(car);
+  }
+  forwardUntilWall(car);
 }
 
 function sensorRight(car) {
-    turnRight(car)
-    let result = sensor(car);
-    turnLeft(car);
+  turnRight(car);
+  let result = sensor(car);
+  turnLeft(car);
 
-    return result;
+  return result;
 }
 
 function firstRight(car) {
-    forwardUntilFreeRight(car)
-    turnRight(car);
-    forwardUntilWall(car);
+  forwardUntilFreeRight(car);
+  turnRight(car);
+  forwardUntilWall(car);
 }
 
 function sensorLeft(car) {
-    turnLeft(car);
-    let result = sensor(car);
-    turnRight(car);
+  turnLeft(car);
+  let result = sensor(car);
+  turnRight(car);
 
-    return result;
+  return result;
 }
 
 function firstLeft(car) {
-    forwardUntilFreeLeft(car)
-    turnLeft(car);  
-    forwardUntilWall(car);
+  forwardUntilFreeLeft(car);
+  turnLeft(car);
+  forwardUntilWall(car);
 }
 
 function zigZag(car) {
-    firstRight(car);
-    turnLeft(car);
-    forward(car);
-    firstLeft(car);       // Start at beginning of path.  Turn right until wall.  Forward until wall.  Turn left until wall.  Forward until wall.  Turn right until wall.  Forward until wall.  Turn left until wall.  Forward until wall.  Turn right until wall.  Forward until wall.  Turn right until wall.  Forward until wall.  Turn left until wall.  Forward until wall.  Turn right until wall.  Forward until wall.  Turn right until wall.  Forward until wall.  Turn left until wall.  Forward until wall.  Turn right unti
+  firstRight(car);
+  turnLeft(car);
+  forward(car);
+  firstLeft(car);
 }
 
 function forwardUntilFreeRight(car) {
-    while(sensorRight(car)) {
-        forward(car);
-    }
+  while (sensorRight(car)) {
+    forward(car);
+  }
 }
 
 function forwardUntilFreeLeft(car) {
-    while(sensorLeft(car)) {
-        forward(car);
-    }
+  while (sensorLeft(car)) {
+    forward(car);
+  }
 }
 
 function secondRight(car) {
-    forwardUntilFreeRight(car);
-    forward(car);
-    firstRight(car);
+  forwardUntilFreeRight(car);
+  forward(car);
+  firstRight(car);
 }
 
 function thirdRight(car) {
-    forwardUntilFreeRight(car);
+  forwardUntilFreeRight(car);
+  forward(car);
+  secondRight(car);
+}
+
+function forwardUntillNthRight(car, nrights) {
+  let i = nrights;
+
+  while (i > 0) {
     forward(car);
-    secondRight(car);
-}
-
-function forwardUntillNthRight(car, nrights) { 
-    let i = nrights;
-
-    while(i > 0) {
-        forward(car);
-        if (!sensorRight(car)) {
-            i--;
-        }
+    if (!sensorRight(car)) {
+      i--;
     }
+  }
 }
 
-function forwardUntillNthLeft(car, nlefts) { 
-    let i = nlefts;
+function forwardUntillNthLeft(car, nlefts) {
+  let i = nlefts;
 
-    while(i > 0) {
-        forward(car);
-        if (!sensorLeft(car)) {
-            i--;
-        }
+  while (i > 0) {
+    forward(car);
+    if (!sensorLeft(car)) {
+      i--;
     }
+  }
 }
-
 
 function fourthRight(car) {
-    forwardUntillNthRight(car, 4);
-    firstRight(car);
+  forwardUntillNthRight(car, 4);
+  firstRight(car);
 }
 
 function fifthLeft(car) {
-    forwardUntillNthLeft(car, 5);
-    firstLeft(car);
+  forwardUntillNthLeft(car, 5);
+  firstLeft(car);
 }
 
 function maze(car) {
-    R(2);
-    L(1);
-    L(2);
-    L(2);
-    R(4);
-    R(1);
-    L(3);
-    forwardUntilWall(car);
+  R(2);
+  L(1);
+  L(2);
+  L(2);
+  R(4);
+  R(1);
+  L(3);
+  forwardUntilWall(car);
 
-    function L(n) {
-        forwardUntillNthLeft(car, n);
-        turnLeft(car);
-    }
+  function L(n) {
+    forwardUntillNthLeft(car, n);
+    turnLeft(car);
+  }
 
-    function R(n) {
-        forwardUntillNthRight(car, n);
-        turnRight(car);
-    }
-
-
-    
-
-} // UC:  forward until wall, turn left, forward until wall, turn right, forward until wall, turn left, forward until wall  - this is not the most efficient way to do it, but it works for now.                                
+  function R(n) {
+    forwardUntillNthRight(car, n);
+    turnRight(car);
+  }
+}
